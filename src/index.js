568e9174665292a04bf1cc8cec7da43fb3a7a3ef
gyr <876156672@qq.com>
2020-06-10 20:44:29 +0800

    脚手架添加less文件且实现babel-plugin-import按需加载

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.less';
 import Parent from './page/demo/parent'
 
 ReactDOM.render(
--- src/page/demo/parent.js
+++ src/page/demo/parent.js
@@ -1,30 +1,33 @@
 import React,{Component} from "react";
 import Children from "./children";
+import { Button } from 'antd';
+import './style.less';
 
 class Parent extends Component{
     constructor(props){
         super(props);
         this.state = {
-					count: 0,
+			count: 0,
         }
     }
 
     render(){
         return (
-        <div>
-            <p>React生命周期,这是父组件</p>
+        <div className="div1">
+            <p className="content">React生命周期,这是父组件</p>
             <button onClick = {this.handleClick.bind(this)}>
 							点击一下
 						</button>
-						<p>{this.state.count}</p>
-						<Children name = {this.state.count}></Children>
+            <Button onClick = {this.handleClick.bind(this)}>Antd点击一下</Button>
+            <p>{this.state.count}</p>
+            <Children name = {this.state.count}></Children>
         </div>
         )
 		}
 		
 		handleClick(){
 			this.setState({
-				count: this.state.count+1
+				count: this.state.count + 1
 			})
 		}
 }
