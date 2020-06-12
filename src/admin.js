2d08c28802aebe29e8e6baafa837511288265357
gyr <876156672@qq.com>
2020-06-12 21:28:35 +0800

    左侧菜单完成，有bug

--- src/admin.js
+++ src/admin.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import { Row, Col } from 'antd';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Menu from "./components/Menu";
+import NavLeft from "./components/NavLeft";
 import './style/common.less';
 
 class Admin extends Component{
@@ -17,7 +17,7 @@ class Admin extends Component{
 			return(
 				<Row className = "container">
 					<Col span={4} className = "Menu">
-						<Menu/>
+						<NavLeft/>
 					</Col>
 					<Col span={20} className = "main">
 						<Header/>
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,47 @@
+import React,{Component} from "react";
+import MenuConfig from './../../config/menuConfig';
+import {Menu} from "antd";
+import "./index.less"
+
+const { SubMenu } = Menu;
+class NavLeft extends Component{
+		组件挂载前
+    componentWillMount(){
+			const menuTreeNode = this.renderMenu(MenuConfig);
+			// 把数据存起来
+			this.setState({
+				menuTreeNode
+			})
+		}
+		// 菜单渲染(递归函数)
+		renderMenu = (data)=>{
+			return data.map((item)=>{
+				if(item.children){
+						return (
+							<SubMenu key={item.key} title={item.title}>
+								{this.renderMenu(item.children)}
+							</SubMenu>
+				)}
+				return <Menu.item title = {item.title} key = {item.key}>
+						        {item.title}
+								</Menu.item>
+		})
+	}
+
+    render(){
+        return(
+            <div>
+                <div className = 'logo'>						
+                    <img src="/assets/logo-ant.svg" alt=""></img>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu theme = "dark">
+										{this.state.menuTreeNode}
+                </Menu>
+            </div>
+        )
+    }
+}
+export default NavLeft;
+
+
